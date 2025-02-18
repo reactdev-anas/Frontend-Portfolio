11651dfaadcdaa40c8b12152e62a2a5161a49767
reactdev-anas <anas.reactdev@gmail.com>
2025-02-18 19:05:27 +0530

    Update Contact.jsx

--- src/components/views/Contact.jsx
+++ src/components/views/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
     };
 
     try {
-      const response = await fetch('http://localhost:5000/send-email', {
+      const response = await fetch('https://backend-portfolio-roan-nine.vercel.app/send-email', {
           method: 'POST',
           headers: {
               'Content-Type': 'application/json',
