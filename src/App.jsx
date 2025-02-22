c5154179d86c094c552d0c6be43b2ae3ec450be0
reactdev-anas <anas.reactdev@gmail.com>
2025-02-22 15:19:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import HeroSection from './components/views/HeroSection'
 import Contact from './components/views/Contact'
 import OurWork from './components/views/OurWork'
 import Testimonials from './components/views/Testimonials'
-import Home from './components/Animation/Home'
 import Services from './components/views/Services'
 import Skills from './components/views/Skills'
 import Footer from './components/views/Footer'
@@ -13,13 +12,8 @@ function App() {
   
   return (
     <>
-    
-    <div className='w-screen h-secreen relative'>
-   
-    <Home/>
     <NavBar/>
-      <HeroSection/>
-    </div>
+    <HeroSection/>
     <OurWork/>
     <Skills/>
     <Services/>
